ff432de2934377f3c4c641f2dbbe48d90762e5e5
DavitGujejiani <gujejiani.daviti@gmail.com>
2021-07-07 12:12:50 +0400

    63. Rendering lists of data

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,41 +2,32 @@ import './Expenses.css';
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from './ExpensesFilter';
+import { useState } from 'react';
 
-function ExpensesItems(props) {
-    const expenses = props.expenses;
+function Expenses(props) {
+    const [filteredYear, setFilteredYear] = useState('2020');
 
-    function filterChangeHandler(selectedOption) {
-        console.log('In Expenses', selectedOption);
+    function filterChangeHandler(filteredYear) {
+        setFilteredYear(filteredYear);
     }
 
     return (
         <div>
             <Card className="expenses">
-                <ExpensesFilter onFilterChange={filterChangeHandler} />
-                <ExpenseItem
-                    title={expenses[0].title}
-                    amount={expenses[0].amount}
-                    date={expenses[0].date}
+                <ExpensesFilter
+                    selected={filteredYear}
+                    onChangeFilter={filterChangeHandler}
                 />
-                <ExpenseItem
-                    title={expenses[1].title}
-                    amount={expenses[1].amount}
-                    date={expenses[1].date}
-                />
-                <ExpenseItem
-                    title={expenses[2].title}
-                    amount={expenses[2].amount}
-                    date={expenses[2].date}
-                />
-                <ExpenseItem
-                    title={expenses[3].title}
-                    amount={expenses[3].amount}
-                    date={expenses[3].date}
-                />
+                {props.items.map((expense) => (
+                    <ExpenseItem
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                ))}
             </Card>
         </div>
     );
 }
 
-export default ExpensesItems;
+export default Expenses;
